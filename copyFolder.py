c2e407468e757c0782126a95cf00da719b52bf3e
giovanni <144000giovanni@gmail.com>
2022-10-17 19:36:59 +1300

    'update'

---
+++ copyFolder.py
@@ -0,0 +1,28 @@
+import json
+import os
+import shutil
+
+source_dir = os.path.join(r"C:\Users\giova\Desktop\trustpay", "nzd")
+
+f = open("currencyCodes.json", "r")
+jsondata = json.loads(f.read())
+del jsondata["currencyCodes"]["NZD"]
+for currency in jsondata["currencyCodes"]:
+  print(currency)
+  currencyfolder = currency.lower()
+  
+  dest_dir = os.path.join(r"C:\Users\giova\Desktop\trustpay", currencyfolder)
+  try:
+    shutil.rmtree(dest_dir)
+  except: 
+    print(dest_dir + " does not exist")
+    shutil.copytree(source_dir, dest_dir)
+    source_file = os.path.join(r"C:\Users\giova\Desktop\trustpay", currencyfolder, "paypal.html")
+    f = open(source_file)
+    html = f.read()
+    f.close()
+    html = html.replace("NZD", currency)
+    print(html)
+    with open(source_file, "w", encoding="utf8") as writer:
+      writer.write(html)
+    
